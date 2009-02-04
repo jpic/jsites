ec985fddb9b5aa08c33213c53d55010d87ee4639
jpic <jamespic@gmail.com>
2009-02-04 16:22:50 +0000

    added generic relation to itbm

--- itbm/models.py
+++ itbm/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils.translation import ugettext as _
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
 
 class Ticket(models.Model):
     title = models.CharField(max_length=100, verbose_name=_(u'title'))
@@ -8,3 +10,11 @@ class Ticket(models.Model):
     description = models.TextField(verbose_name=_(u'description'))
     def __unicode__(self):
         return '#%s: %s' % (self.pk, self.title)
+
+class Foo(models.Model):
+    name = models.CharField(max_length=200, null=True, blank=True)
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
+    def __unicode__(self):
+        return self.name
