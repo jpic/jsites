20f5821b903f07f8387cb6b7adea6869df8f44a3
jpic <jamespic@gmail.com>
2009-03-30 11:10:03 +0000

    added example

--- testapp/site.py
+++ testapp/site.py
@@ -1,10 +1,23 @@
 import jsites
 import models
+from jadmin import menus
+
+class CarSite(jsites.ControllerWrapper):
+    def get_menu(self):
+        menu = menus.Menu()
+        site = menu.add('siteitem', 'siteurl')
+        site.add('sitesubitem', 'sitesuburl')
+        return menu
 
 class CarController(jsites.Controller):
+    def get_menu(self):
+        menu = menus.Menu()
+        controller = menu.add('controlleritem', 'controllerurl')
+        controller.add('controllersubitem', 'url')
+        return menu
     name = 'car'
     urlname = 'cars'
     content_class = models.Car
 
-site = jsites.ControllerWrapper('sitename')
+site = CarSite('sitename')
 site.register(CarController('carlol'))
