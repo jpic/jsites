90dec660968586f6c043bcda2452446871f6cea8
jpic <jamespic@gmail.com>
2009-04-02 18:03:10 +0000

    installed databrowse, admindoc and structure, refactored urls

--- urls.py
+++ urls.py
@@ -1,18 +1,38 @@
 from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+# ugly hack
+urlpatterns = patterns('',)
 
+from django.contrib import admin
+# ugly hack that has bugs pending for months (can't re-register ...)
+admin.autodiscover()
+urlpatterns += patterns('',
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
+)
+
+from django.contrib import databrowse
+# ugly hack to force the user using a singleton
+from itbm import databrowse
+urlpatterns += patterns('',
+    (r'^databrowse/(.*)', databrowse.site.root),
+)
+
+# getting started
+import jsites
+urlpatterns += patterns('',
+    # usage:
+    # (r'^app_label/', include(jsites.ControllerNode.factory('app_label').urls)),
+    # example: 
+    (r'^itbm/', include(jsites.ControllerNode.factory('itbm').urls)),
+)
+
+# single controller, multi-site example
 from itbm.sites import site as itbm
-
-urlpatterns = patterns('',
-    # Example:
-    (r'^%s/' % itbm.urlname, include(itbm.get_urls()))
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+urlpatterns += patterns('',
+    # usage:
+    # (r'^staff/',    include(staff.urls)),
+    # (r'^partners/', include(partners.urls)),
+    # example, main site controller:
+    (r'^%s/' % itbm.urlname, include(itbm.urls)),
 )
