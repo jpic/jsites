a6eda325db44ed73e60e8f75af38417ec0852a6b
jpic <jamespic@gmail.com>
2009-04-08 15:29:40 +0000

    media cleaning and more fixes in sites.py

---
+++ itbm/media/js/ticket.constraints.js
@@ -0,0 +1,28 @@
+alert('foo');
+/*
+ui.addConstraint([
+        'depot_arrete_lotir',
+        'obtention_arrete_lotir',
+        'bornage_prevu_le',
+        'bornage_realise_le',
+        'travaux_prevu_le',
+        'travaux_realises_le',
+        'livraison_prevue',
+        'livraison_prevue_le',
+        'livraison_realisee_le',
+        'debut_construction'
+    ],
+    'type',
+    'Lotissement',
+    jpicFieldValueEqual
+);
+
+function maj()
+{
+    ui.update();
+}
+
+$('#id_type').change(maj);
+maj();
+jpicTabManager.select('id_mandat');
+*/ 
